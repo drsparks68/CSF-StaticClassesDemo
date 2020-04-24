b72de4a96a9a79fbce69001919ad933227becdb7
Derek Sparks <drsparks@gmail.com>
2020-04-23 19:16:19 -0700

    Initial Import

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = RequestData.GetAString("What is your first name?");
+            UserMessages.ApplicationStartMessage(firstName);
+
+            double x = RequestData.GetADouble("Enter the first number: ");
+            double y = RequestData.GetADouble("Enter the second number: ");
+
+            double result = CalculateData.Add(x, y);
+
+            UserMessages.PrintResultMessage($"The sum of { x } and { y } is { result }");
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ConsoleUI/UserMessages.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class UserMessages
+    {
+        public static void ApplicationStartMessage(string firstName)
+        {
+            Console.WriteLine("Welcome to the Static Class demo app");
+
+            int hourOfDay = DateTime.Now.Hour;
+            if (hourOfDay > 12)
+            {
+                Console.WriteLine($"Good Morning, { firstName }!");
+            }
+            else if (hourOfDay < 18)
+            {
+                Console.WriteLine($"Good Afternoon, { firstName }!");
+            }
+            else
+            {
+                Console.WriteLine($"Good Evening, { firstName }!");
+            }
+        }
+
+        public static void PrintResultMessage(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine();
+            Console.WriteLine("Thanks for using this calculator");
+        }
+
+    }
+}
